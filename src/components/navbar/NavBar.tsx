d2914892076aa0dec59f16208f440fe3045a691b
Gakuraa <gabrieliwakura924@gmail.com>
2024-01-31 14:12:14 -0300

    Criação dos componentes Navbar, Footer e model de postagem

---
+++ src/components/navbar/NavBar.tsx
@@ -0,0 +1,25 @@
+import { Link, useNavigate } from 'react-router-dom'
+
+function Navbar() {
+  let navigate = useNavigate()
+
+  let navbarComponent
+  
+
+  return (
+    <>
+     <div className='w-full bg-cyan-400 text-white flex justify-center py-4'>
+          <div className="container flex justify-between text-lg">
+          <Link to='/home' className='text-2xl font-bold uppercase'>Farmacia IWAKURA</Link>
+
+            <div className='flex gap-4'>
+            <Link to='/categorias' className='hover:underline'>Categorias</Link>
+            <Link to='/cadastroCategoria' className='hover:underline'>Cadastrar Categoria</Link>             
+            </div>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
